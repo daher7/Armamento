1fd63a4c3f3542d2d7f755335d9abd34d0850567
daher7 <daher7@gmail.com>
2018-10-19 11:17:47 +0200

    Raycast

--- Assets/_MyGameAssets/Scripts/FrancotiradorScript.cs
+++ Assets/_MyGameAssets/Scripts/FrancotiradorScript.cs
@@ -5,6 +5,39 @@ using UnityEngine;
 public class FrancotiradorScript : MonoBehaviour {
 
     [SerializeField] Transform puntoDisparo;
+    [SerializeField] Camera miCamara;
+    [SerializeField] GameObject mirilla;
+    bool apuntando = false;
+    float currentFOV;
+    float minFOV = 25;
+    float maxFOV;
+
+    private void Start() {
+        maxFOV = miCamara.fieldOfView;
+        currentFOV = miCamara.fieldOfView;
+    }
+
+    private void Update() {
+        if (Input.GetMouseButtonDown(1)) {
+            apuntando = true;
+            mirilla.SetActive(true);
+        }
+        if (Input.GetMouseButtonUp(1)) {
+            apuntando = false;
+        }
+        if (apuntando) {
+            currentFOV -= 1;
+            // Cogemos el valor maximo de dos valores
+            currentFOV = Mathf.Max(currentFOV, minFOV);
+        }
+        if (apuntando == false) {
+            currentFOV += 1;
+            // Cogemos el valor maximo de dos valores
+            currentFOV = Mathf.Min(currentFOV, maxFOV);
+        }
+
+        miCamara.fieldOfView = currentFOV;
+    }
 
     private void OnMouseDown() {
         // Declaramos el Vector
@@ -15,9 +48,14 @@ public class FrancotiradorScript : MonoBehaviour {
         RaycastHit hitInfo;
         // Lanzamos el Rayo
         bool impactoConseguido = Physics.Raycast(rayo, out hitInfo, 25);
-
+        // Evaluamos el impacto
         if (impactoConseguido) {
             print(hitInfo.collider.gameObject.name);
+            Debug.DrawLine(
+                puntoDisparo.position,
+                hitInfo.collider.transform.position,
+                Color.red, 
+                5);
         } else {
             print("AGUA");
         }
