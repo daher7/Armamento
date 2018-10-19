5d7228f9986930dbb5628afc995827f3da12ceb8
daher7 <daher7@gmail.com>
2018-10-19 10:02:31 +0200

    FrancoTirador

---
+++ Assets/_MyGameAssets/Scripts/FrancotiradorScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FrancotiradorScript : MonoBehaviour {
+
+    [SerializeField] Transform puntoDisparo;
+
+    private void OnMouseDown() {
+        // Declaramos el Vector
+        Vector3 forward = puntoDisparo.forward;
+        // Creamos el rayo
+        Ray rayo = new Ray(puntoDisparo.position, forward);
+        // Declaramos el objeto que recoge el impacto
+        RaycastHit hitInfo;
+        // Lanzamos el Rayo
+        bool impactoConseguido = Physics.Raycast(rayo, out hitInfo, 25);
+
+        if (impactoConseguido) {
+            print(hitInfo.collider.gameObject.name);
+        } else {
+            print("AGUA");
+        }
+    }
+}
